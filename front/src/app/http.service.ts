1b8b93b4bfe06f9998621cde97153db3e33eab38
Michel Palourdio <mpalourdio@gmail.com>
2025-01-26 21:22:45 +0100

    chore: fix lint

--- front/src/app/http.service.ts
+++ front/src/app/http.service.ts
@@ -10,7 +10,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { Answer } from "./model/answer";
 
 @Injectable({
