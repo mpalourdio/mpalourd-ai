f5869c6f71cff71003afbe2fb7213933b8c7154f
Michel Palourdio <mpalourdio@gmail.com>
2025-01-29 22:27:20 +0100

    chore: cleanup unused imports

--- src/main/kotlin/com/mpalourdio/projects/mpalourd_ai/controller/OpenAiController.kt
+++ src/main/kotlin/com/mpalourdio/projects/mpalourd_ai/controller/OpenAiController.kt
@@ -14,7 +14,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.ai.autoconfigure.openai.OpenAiChatProperties
 import org.springframework.ai.chat.client.ChatClient
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.web.csrf.CsrfToken
 import org.springframework.web.bind.annotation.*
 import java.io.File
