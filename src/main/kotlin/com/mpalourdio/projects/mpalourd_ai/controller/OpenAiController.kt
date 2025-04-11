09df44898a35b980a3af39c039af5cfea81d9485
Michel Palourdio <mpalourdio@gmail.com>
2025-04-11 08:13:10 +0200

    feat: Move from milestones to snapshots

--- src/main/kotlin/com/mpalourdio/projects/mpalourd_ai/controller/OpenAiController.kt
+++ src/main/kotlin/com/mpalourdio/projects/mpalourd_ai/controller/OpenAiController.kt
@@ -17,8 +17,8 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor
-import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor
 import org.springframework.ai.chat.memory.InMemoryChatMemory
 import org.springframework.ai.vectorstore.SearchRequest
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore
